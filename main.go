aef77d96ecee21c96c836fd64c97c94a23ea90f9
Damilola <gabrielpromise18@gmail.com>
2021-09-16 17:18:57 +0100

    rest api i think

---
+++ main.go
@@ -0,0 +1,22 @@
+package main 
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"log"
+)
+
+var schools []strings
+
+func main (){
+	r := gin.Default()
+	r.GET("/",schools)
+	r.POST("/school",newschools)
+	r.Run("8080")
+}
+
+func newschools(c *gin.context){
+	school := c.PostForm("school")
+	schools := appending(schools, school)
+	c.String(http.StatusCreated, c.FullPath()+"/"+strconv.Itoa(len(todos)-1))
+}
