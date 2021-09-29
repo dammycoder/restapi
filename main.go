01e5c163681641a207e5c688f09381a059da1e48
dammycoder <84012871+dammycoder@users.noreply.github.com>
2021-09-29 11:12:57 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,22 +1,80 @@
-package main 
+package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
-	"log"
+
+	"github.com/gin-gonic/gin"
 )
 
-var schools []strings
-
-func main (){
+func main() {
 	r := gin.Default()
-	r.GET("/",schools)
-	r.POST("/school",newschools)
-	r.Run(":8080")
+
+	r.GET("/todo-list", listTodoList)
+	r.POST("/todo-list", createTodoList)
+	r.DELETE("/todo-list/:todoListID", deleteTodoList)
+	r.PATCH("/todo-list/:todoListID", patchTodoList)
+	r.GET("/todo-list/:todoListID", showTodoList)
+	r.GET("/todolist/:todolistID/:todoITEM",todoitems)
+	r.POST("/todolist/:todolistID/:todoITEM",addtodoitem)
+	r.PATCH("/:todoITEM",marktodoitem)
+	r.Run("127.0.0.1:8888")
 }
 
-func newschools(c *gin.context){
-	school := c.PostForm("school")
-	schools := append(schools, school)
-	c.String(http.StatusCreated, c.FullPath()+"/"+strconv.Itoa(len(todos)-1))
+func listTodoList(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, map[string]interface{}{
+		"data": []string{},
+	})
+}
+
+func createTodoList(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, map[string]interface{}{
+		"id": 1,
+	})
+}
+
+func deleteTodoList(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, map[string]interface{}{
+		"id": ctx.Param("todoListID"),
+	})
+}
+
+func patchTodoList(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, map[string]interface{}{
+		"id": ctx.Param("todoListID"),
+	})
+}
+
+func showTodoList(ctx *gin.Context) {
+
+	todoListID := ctx.Param("todoListID")
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"id": todoListID,
+	})
+}
+
+func todoitems (ctx *gin.Context){
+
+	todoItems := ctx.Param("todoITEM")
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"data" : todoItems,
+	})
+}
+
+func addtodoitem(ctx *gin.Context){
+	todoItems := ctx.Param("todoITEM")
+
+	ctx.JSON(http.StatusOK,gin.H{
+		"id": todoItems,
+	})
+}
+
+func marktodoitem(ctx *gin.Context){
+
+	todoItems := ctx.Param("todoITEM")
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"id":todoItems,
+	})
 }
